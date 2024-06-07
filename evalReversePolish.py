bea97ad0d04c5a5ea1369dc5f41c627206cb1e6f
Shash <shashgk23@gmail.com>
2024-06-06 22:11:55 -0400

    finished probs

---
+++ evalReversePolish.py
@@ -0,0 +1,1 @@
+def evalRPN(tokens: List[str]) -> int:
---
+++ generateParenthesis.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def generateParenthesis(n: int) -> List[str]:
+    
+    # base case
+    if n == 1:
+        return ["()"]
+    
+    
+    
+    return None
