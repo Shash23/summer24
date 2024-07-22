e594bfca51da17dcb9f41a30161cb22f461216d8
Shash <shashgk23@gmail.com>
2024-07-21 22:23:32 -0400

    rust introooooo

---
+++ rust/hello_cargo/target/debug/deps/hello_cargo-c40274329a7a9646.d
@@ -0,0 +1,5 @@
+/Users/shashghevde/Documents/summer24/rust/hello_cargo/target/debug/deps/hello_cargo-c40274329a7a9646: src/main.rs
+
+/Users/shashghevde/Documents/summer24/rust/hello_cargo/target/debug/deps/hello_cargo-c40274329a7a9646.d: src/main.rs
+
+src/main.rs:
---
+++ rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/shashghevde/Documents/summer24/rust/hello_cargo/target/debug/hello_cargo: /Users/shashghevde/Documents/summer24/rust/hello_cargo/src/main.rs
