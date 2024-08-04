387d658eabb833256d3a843b220e371792ef7025
Shash <shashgk23@gmail.com>
2024-08-04 12:55:05 -0400

    better two sum

--- twoSum.py
+++ twoSum.py
@@ -6,3 +6,22 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
             complement = target - nums[i]
             if complement in hashmap and hashmap[complement] != i:
                 return [i, hashmap[complement]]
+            
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        hash_map = {}
+
+        for i in range(len(nums)):
+
+            temp = target - nums[i]
+
+            if temp in hash_map:
+                return [i, hash_map[temp]]
+
+            hash_map[nums[i]] = i
+
+            
+
+
+        
+        
