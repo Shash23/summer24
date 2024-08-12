8d798260d3b922191683fa62e6de26b636629de0
Shash <shashgk23@gmail.com>
2024-08-12 13:45:17 -0400

    more efficient

--- maxDepth.py
+++ maxDepth.py
@@ -1,11 +1,22 @@
-def maxDepth(self, root: Optional[TreeNode]) -> int:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
 
-        if root is None:
+        if not root:
             return 0
 
-        else:
+        # remember the max function
+        return max(self.maxDepth(root.right), self.maxDepth(root.left)) + 1
 
-            left_height = self.maxDepth(root.left)
-            right_height = self.maxDepth(root.right)
 
-            return max(left_height, right_height) + 1
+            
+
+
+
+
+        
