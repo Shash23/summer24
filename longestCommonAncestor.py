c843094ecf3198c5f4cc674e85e2bc637e359f67
Shash <shashgk23@gmail.com>
2024-08-13 14:11:34 -0400

    answer attempt

--- longestCommonAncestor.py
+++ longestCommonAncestor.py
@@ -2,7 +2,37 @@
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
+#         self.left = Nonea# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
 #         self.left = None
+#         self.right = None
+
+class Solution:
+
+    self.ans = None
+
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        def traverse(curr):
+
+            if not root:
+                return None
+
+            # Does the left subtree contain either p or q?
+            left_trav = lowestCommonAncestor(curr.left, p, q)
+
+            # Does the right subtree contain either p or q?
+            right_trav = lowestCommonAncestor(curr.right, p, q)
+
+            # Is the current node itself either p or q?
+            pa = curr == p or curr == q
+
+            if pa + left_trav + right_trav >= 2:
+                self.ans = curr
+
+            return pa 
 #         self.right = None
 
 class Solution(object):
