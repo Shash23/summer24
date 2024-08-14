662331ecfe7ab2e7934d7c00043b129ac4610d36
Shash <shashgk23@gmail.com>
2024-08-14 18:39:55 -0400

    going over basics

---
+++ reverse_linked_list.py
@@ -0,0 +1,12 @@
+def reverseLinkedList(head: Optional[ListNode]) -> Optional[ListNode]:
+    
+    prev, curr = None, head
+    
+    while curr:
+        
+        temp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = temp
+        
+    return prev
