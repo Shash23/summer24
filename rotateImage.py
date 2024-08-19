c2c44b48b61adff07af8223fed4ec597bd63c9fc
Shash <shashgk23@gmail.com>
2024-08-19 11:08:37 -0400

    rotating 90

---
+++ rotateImage.py
@@ -0,0 +1,21 @@
+def rotate(matrix: List[List[int]]) -> None:
+    
+    n = len(matrix)
+    
+    # remember to not overlap
+    
+    # transpose
+    for i in range(n):
+        for j in range(i+1, n):
+            
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            
+    # reflect
+    '''
+    for i in range(n):
+        for j in range(n // 2):
+            matrix[i][j], matrix[i][j - 1] = (matrix[i][-j-1], matrix[i][j],)
+    '''
+    
+    for i in range(n):
+        matrix[i].reverse()
